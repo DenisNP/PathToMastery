0226280abd22d3d57dd0e85f8a57a58adbc0bdec
Denis Peshekhonov <denis@peshekhonov.ru>
2020-08-29 21:49:43 +0300

    frontend start

---
+++ ptm-front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+@font-face {
+    font-family: 'Maler';
+    src: url('./assets/Maler.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.c-font {
+  font-family: 'Maler', sans-serif;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
